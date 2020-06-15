60aaa92e0db100193475acc4587651e4c29375e5
kcorn1982 <kcorn1982@gmail.com>
2020-06-15 11:10:52 +0100

    hard coded private network variable to get things moving for a demo need to change

--- imdb_db_data_management/sql_management/queries/imdb_query_manager.py
+++ imdb_db_data_management/sql_management/queries/imdb_query_manager.py
@@ -4,14 +4,17 @@ import os
 class ImdbQueryManager:
 
     def __init__(self):
-        self.db_conn = ImdbDBConnManager(host=os.getenv("PG_ENV"))
+        self.db_conn = ImdbDBConnManager(host="192.168.33.10")
 
     def get_all_movie_data(self):
-        self.db_conn.execute_query("SELECT * FROM moviedata")
+        self.db_conn.execute_query("SELECT * FROM movies")
         return self.db_conn.return_data()
 
 
     def search_film_by_name(self, string_in_title):
-        self.db_conn.execute_query("SELECT * FROM moviedata WHERE LOWER(movie_title) LIKE LOWER('%" + string_in_title + "%');")
+        self.db_conn.execute_query("SELECT * FROM movies WHERE LOWER(movie_title) LIKE LOWER('%" + string_in_title + "%');")
         return self.db_conn.return_data()
 
+if __name__ == '__main__':
+    x = ImdbQueryManager()
+    print(x.get_all_movie_data())
